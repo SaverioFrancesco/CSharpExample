6e73908c18a73f8e70964fc756c0616208659d38
Francesco Saverio Comisso <mitramdhir@gmail.com>
2019-03-20 09:21:53 +0100

    Product

--- MainExample/MainExample/Program.cs
+++ MainExample/MainExample/Program.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+using System.Collections.Generic;
+
 namespace MainExample
 {
     class Program
@@ -12,8 +15,37 @@ namespace MainExample
         {
 
             Console.WriteLine("We made a console app");
+            Console.WriteLine(value: Product.GetSampleProducts()[0].ToString());
             Console.ReadKey();
 
         }
+    }
+}
+
+
+public class Product
+{
+    readonly string name;
+    public string Name { get { return name; } }
+    readonly decimal price;
+    public decimal Price { get { return price; } }
+    public Product(string name, decimal price)
+    {
+        this.name = name;
+        this.price = price;
+    }
+    public static List<Product> GetSampleProducts()
+    {
+        return new List<Product>
+        {
+            new Product(name: "West Side Story", price: 9.99m),
+            new Product(name: "Assassins", price: 14.99m),
+            new Product(name: "Frogs", price: 13.99m),
+            new Product(name: "Sweeney Todd", price: 10.99m)
+        };
+    }
+    public override string ToString()
+    {
+        return string.Format("{0}: {1}", name, price);
     }
 }
