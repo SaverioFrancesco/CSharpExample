ea5dd28a4777b05e78769311dde202b6e6466632
Francesco Saverio Comisso <mitramdhir@gmail.com>
2019-03-20 09:44:50 +0100

    Sorting1

--- MainExample/MainExample/Program.cs
+++ MainExample/MainExample/Program.cs
@@ -15,7 +15,21 @@ namespace MainExample
         {
 
             Console.WriteLine("We made a console app");
-            Console.WriteLine(value: Product.GetSampleProducts()[0].ToString());
+            // Console.WriteLine(value: Product.GetSampleProducts()[0].ToString());
+            Console.ReadKey();
+
+
+            List<Product> products = Product.GetSampleProducts();
+            Predicate<Product> test = delegate (Product p) { return p.Price > 10m; };
+            List<Product> matches = products.FindAll(test);
+            Action<Product> print = Console.WriteLine;
+            matches.ForEach(print);
+
+            foreach (Product product in products.Where(p => p.Price > 10))
+            {
+                Console.WriteLine(product);
+            }
+
             Console.ReadKey();
 
         }
@@ -39,9 +53,10 @@ public class Product
         return new List<Product>
         {
             new Product(name: "West Side Story", price: 9.99m),
-            new Product(name: "Assassins", price: 14.99m),
+            new Product(name: "Assassins", price: 04.99m),
             new Product(name: "Frogs", price: 13.99m),
-            new Product(name: "Sweeney Todd", price: 10.99m)
+            new Product(name: "Sweeney Todd", price: 19.99m),
+            new Product(name: "Esteldor", price: 10.99m)
         };
     }
     public override string ToString()
